658df3978bad3b06853c6d54f4d8f9b8306f835b
Taha Hancı <taha.hanci@std.bogazici.edu.tr>
2025-03-05 16:06:46 +0300

    Inefficient solution for ContainsDuplicate

--- src/easy/ContainsDuplicate.java
+++ src/easy/ContainsDuplicate.java
@@ -27,4 +27,23 @@ public class ContainsDuplicate {
 
         return false;
     }
+
+    public boolean containsDuplicateWithInefficientWay(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1};
+        System.out.println(containsDuplicate.containsDuplicate(nums));
+        System.out.println(containsDuplicate.containsDuplicateWithInefficientWay(nums));
+    }
 }
