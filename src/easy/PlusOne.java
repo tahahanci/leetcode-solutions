e9c42eea0165e6020d2a838371f2acf766b64dc9
Taha Hancı <taha.hanci@std.bogazici.edu.tr>
2025-03-05 17:00:01 +0300

    66. Plus One

---
+++ src/easy/PlusOne.java
@@ -0,0 +1,30 @@
+package easy;
+
+import java.util.Arrays;
+
+public class PlusOne {
+
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] newDigits = new int[n + 1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
+
+    public static void main(String[] args) {
+        PlusOne plusOne = new PlusOne();
+        int[] myArray = {1, 2, 3, 4};
+        myArray = plusOne.plusOne(myArray);
+
+        System.out.println(Arrays.toString(myArray));
+    }
+}
