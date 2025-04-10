e6bf802844cf3ab17124646c995c381c08473eb2
Taha Hancı <taha.hanci@std.bogazici.edu.tr>
2025-04-11 00:44:58 +0300

    Add RemoveElements solution in Java
    Implement the RemoveElements problem, which removes all occurrences of a given value from an integer array in-place and returns the count of remaining elements. Includes problem details, examples, and a main method for testing.

---
+++ src/easy/RemoveElements.java
@@ -0,0 +1,48 @@
+package easy;
+
+public class RemoveElements {
+
+    /*
+    Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+    Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+    Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+    Return k.
+
+    Example 1:
+
+    Input: nums = [3,2,2,3], val = 3
+    Output: 2, nums = [2,2,_,_]
+    Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+    It does not matter what you leave beyond the returned k (hence they are underscores).
+
+    Example 2:
+
+    Input: nums = [0,1,2,2,3,0,4,2], val = 2
+    Output: 5, nums = [0,1,4,0,3,_,_,_]
+    Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+    Note that the five elements can be returned in any order.
+    It does not matter what you leave beyond the returned k (hence they are underscores).
+     */
+
+    public static int removeElements(int[] nums, int val) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index++] = nums[i];
+            }
+        }
+
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 2, 1, 3, 5};
+        System.out.println(removeElements(array, 1));
+    }
+}
